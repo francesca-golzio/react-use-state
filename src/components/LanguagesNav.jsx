1a6e96cb2e874a42a662076de4ca1593ca399178
Francesca Golzio <francescagolzio@gmail.com>
2026-03-14 16:46:33 +0100

    update state on click
    missing: update UI

--- src/components/LanguagesNav.jsx
+++ src/components/LanguagesNav.jsx
@@ -1,26 +1,28 @@
 import languages from '../assets/languages'
+import { useState } from 'react'
 
-function LanguagesNav() {
-
+export default function LanguagesNav() {
+  let [stato, aggiornaStato] = useState(0);
 
   return (
-
+    
     <>
-
       {
         languages.map((language) => {
           const { id, title } = language;
-
+          
           return (
-            <button key={id}>
+            <button 
+            key={id}
+              onClick={() => { aggiornaStato(stato = id); console.log(stato, id); }}>
               {title}
             </button>
           )
         })
+      
       }
-
     </>
   )
 }
 
-export default LanguagesNav
+
