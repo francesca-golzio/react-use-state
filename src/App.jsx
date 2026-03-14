33b488383cf019a8c61139c525a1baf8d4dc44b6
Francesca Golzio <francescagolzio@gmail.com>
2026-03-14 18:47:19 +0100

    fix button event onClick

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import languages from './assets/languages'
 import { useState } from 'react'
 
 function App() {
-  let [attivo, aggiornaAttivo] = useState(0);
+  let [attivo, aggiornaAttivo] = useState('');
 
   return (
     <>
@@ -13,13 +13,20 @@ function App() {
         const { id, title } = language;
 
         return (
-          <button key={id} onClick={() => { aggiornaAttivo(attivo = (id - 1)) }}>{title}</button>
+          <button key={id} onClick={() => { aggiornaAttivo(attivo = (id)); console.log(attivo, id); }}>{title}</button>
         )
       })}
 
       <div className="card">
-        <h2>{languages[attivo].title}</h2>
-        <p>{languages[attivo].description}</p>
+        {
+          attivo ?
+            <div className="card-body">
+              <h2>{languages[attivo - 1].title}</h2>
+              <p>{languages[attivo - 1].description}</p>
+            </div>
+            :
+            'nessun linguaggio selezionato'
+        }
       </div>
 
 
