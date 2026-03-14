fbfbdc354a5f4ca48a926919a0f3b082e33206f3
Francesca Golzio <francescagolzio@gmail.com>
2026-03-14 13:14:22 +0100

    add languages button list

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-
+import languages from './assets/languages'
 
 function App() {
-  
+
 
   return (
+
     <>
-      
+
+      {
+        languages.map((language) => {
+          const { id, title } = language;
+
+          return (
+            <button key={id}>
+              {title}
+            </button>
+          )
+        })
+      }
+
     </>
   )
 }
